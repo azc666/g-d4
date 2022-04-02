13b8d274a2ab7f4b9440e7b72a62f2bd1758f000
Allen Cohen <azc666@gmail.com>
2022-04-02 15:55:58 -0400

    initial commit for responsive

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,36 +1,45 @@
 <x-home-layout>
 
-<x-slot name="title">About Us</x-slot>
+  <x-slot name="title">About Us</x-slot>
 
-  <div class="flex p-12 mt-20 mx-20 bg-gray-100 bg-opacity-60 background-trans rounded-lg shadow-lg">
+  <div class="flex p-12 mt-2 sm:mt-10 mx-8 sm:mx-20 bg-gray-100 bg-opacity-60 background-trans rounded-lg shadow-lg">
 
     <div class="w-1 sm:w-1/4">
       <img class="w-0 md:w-full mr-12" src="/assets/posters/homeimg.jpg" alt="">
     </div>
 
     <div class="w-full sm:w-3/4">
-      <h1 class="text-4xl text-gray-800 sm:mb-4 sm:pl-12 text-center sm:text-left font-chicle">
+      <h1 class="text-4xl text-gray-800 mb-2 sm:mb-4 -mt-6 sm:mt-0 sm:pl-12 text-center sm:text-left font-chicle">
         Our Story
       </h1>
-      <p class="text-base sm:text-xl text-gray-700 sm:ml-12 mb-4 sm:mr-6 font-myriad">
-        Graphics + Design was formed in 1987 to provide dependable, personalized graphic services to the Tampa Bay
-        business community. We are proud of our 35 years experience with commercial printing, and we continue to
-        maintain the highest quality production standards.</p>
-      <p class="text-base sm:text-xl text-gray-700 sm:ml-12 mb-4 sm:mr-6 font-myriad">
-        Whether you are a designer, advertising agency, local business or national corporation, our personable and
-        knowledgeable staff can assist with your image, and extend your outreach to a targeted clientel.</p>
-      <p class="text-base sm:text-xl text-gray-700 sm:ml-12 mb-4 sm:mr-6 font-myriad">
-        Throughout the years our philosophy has remained unchanged: treat your project as our own. With an emphasis on
-        service, we strive for the best quality possible, at a fair price, and timely delivery.</p>
-      <p class="text-base sm:text-xl text-gray-700 sm:ml-12 mb-4 sm:mr-6 font-myriad">
-        See what our clients think of us on the <span
-          class="sm:text-xl text-semibold bg-teal-300 border-teal-800 border-2 rounded-lg p-1 pl-2 pr-2 text-teal-900 hover:bg-teal-500 hover:text-teal-200">
-          <a href="/testimonials"><span class="xxs:leading-loose sm:leading-normal"> Testimonials</span></a></span>
-        page.</p>
-      <p class="font-semibold sm:text-xl text-gray-700 text-semibold sm:ml-12 sm:mr-6 font-myriad">
+      <div class="text-lg sm:text-xl text-gray-700 sm:ml-12 mb-4 -mr-4 sm:mr-6 -ml-2 font-myriad">
+        <p class="pb-4 first-letter:uppercase first-line:tracking-wide
+  first-letter:text-7xl first-letter:font-bold first-letter:text-gray-700
+  first-letter:mr-3 first-letter:float-left">
+          Graphics + Design was formed in 1987 to provide dependable, personalized graphic services to the Tampa Bay
+          business community. We are proud of our 35 years experience with commercial printing, and we continue to
+          maintain the highest quality production standards.
+        </p>
+        <p class="pb-4">
+          Whether you are a designer, advertising agency, local business or national corporation, our personable and
+          knowledgeable staff can assist with your image, and extend your outreach to a targeted clientel.
+        </p>
+        <p class="pb-4">
+          Throughout the years our philosophy has remained unchanged: treat your project as our own. With an emphasis on
+          service, we strive for the best quality possible, at a fair price, and timely delivery.
+        </p>
+        <p>
+          See what our clients think of us on the <span
+            class="sm:text-xl text-semibold bg-teal-300 border-teal-800 border-2 rounded-lg p-1 pl-2 pr-2 text-teal-900 hover:bg-teal-500 hover:text-teal-200">
+            <a href="/testimonials"><span class="leading-loose sm:leading-normal pt-2"> Testimonials</span></a></span>
+          page.
+        </p>
+      </div>
+
+      <p class="font-semibold sm:text-xl text-gray-700 italic sm:ml-12 sm:mr-6 font-myriad">
         Graphics + Design is your go-to, full service, print and mail resource!</p><br>
       <p
-        class="font-semibold text-center sm:text-2xl text-gray-700 text-semibold sm:ml-12 mb-4 md:mb-16 sm:mr-6 font-myriad">
+        class="font-bold text-center sm:text-4xl text-gray-700 sm:ml-12 mb-4 sm:mb-12 sm:mr-6 sm:pt-8 font-myriad">
         Graphics + Design<br><span class="italic sm:text-xl">Imaging at the Speed of Business</span> </p>
     </div>
   </div>

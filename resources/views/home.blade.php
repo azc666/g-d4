11083d7289ce5662bf1d58860fa22c60f94d62a3
Allen Cohen <azc666@gmail.com>
2022-04-04 15:47:13 -0400

    aligned home, mobile & desk, cap G

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,17 +18,22 @@
           <h3 class="mt-0 text-2xl sm:text-3xl text-center font-myriad italic">Imaging at the Speed of Business</h3>
         </div>
         <div class="pb-2 flex">
-          <div class="mx-auto sm:w-3/4 p-2 mt-5 sm:mt-8 bg-gray-50 bg-opacity-50 border rounded-lg shadow-2xl">
+          <div class="mx-auto sm:w-3/4 max-w-5xl p-2 mt-5 sm:mt-8 bg-gray-50 bg-opacity-50 border rounded-lg shadow-2xl">
             {{-- <a target="_blank" href="{{ asset('assets/HolidaySchedule2021_22.png') }}"> --}}
               <img class="w-auto mx-auto object-cover rounded" src="/assets/GDBuilding2.png" alt="G+D Home Office">
               {{-- </a> --}}
+
+
             <div class="flex justify-between">
-              <div class="block pt-2 pl-6 mb-0 text-sm sm:text-base text-gray-800 font-bold">Hours: M-F &nbsp; 9am-5pm</div>
+              <div class="block pt-2 pl-2 sm:pl-6 mb-0 text-sm sm:text-base text-gray-800 font-bold ">Hours: M-F 9am-5pm</div>
               {{-- <div class="block pt-2 pr-2 mb-0 text-sm sm:text-base text-gray-800 font-bold">Hours: M-F 9:30am -
                 5pm <span class="text-red-500 font-bold"> - Office Closed for Thanksgiving 11/25-11/26</span></div> --}}
-              <div class="pt-2 pr-6 mb-0 text-right text-sm sm:text-base text-gray-800 font-bold">The Home Office -
-                Tampa, FL</div>
+              <div
+                class="pt-2 pr-2 sm: mb-0 tracking-tight sm:tracking-normal text-center sm:text-right text-sm sm:text-base text-gray-800 font-bold flex-col-2">
+                The Home Office - Tampa, FL
+              </div>
             </div>
+
 
           </div>
         </div>
